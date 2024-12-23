2d541cbdc4cdd57de2975bdd3cdae09b73e485b1
Artem <artem.belykh.ru@gmail.com>
2024-12-23 13:16:34 +0200

    Initial commit

---
+++ practiseBlazor/DataAccess/Model/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace practiseBlazor.Models
+{
+    public class Vehicle
+    {
+        [Key] public long Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Capacity { get; set; }
+        public string LicensePlate { get; set; }
+        public int FuelRate { get; set; }
+        public ICollection<Waybill>? Waybills { get; set; }
+    }
+}
